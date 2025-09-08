5b92e58b1214948557360731e53d0af2917095e5
Dan Gilleland <dgilleland@nait.ca>
2025-09-08 11:37:51 -0600

    Done first demo

--- src/A/review-break-activities-example-START/js/index.js
+++ src/A/review-break-activities-example-START/js/index.js
@@ -16,15 +16,21 @@ const handleSubmit = function(evt) {
     const textInput = targetForm.elements['new-topic'];
     console.log('The user typed:', textInput.value);
 
+    // const help = document.querySelector('div.invalid-feedback');
+
     if(textInput.value.trim() !== '') {
-        // Good, I have text...
+        // Good, I have text... 🚀
         const list = document.querySelector('ul');
         list.innerHTML += `<li>${textInput.value.trim()}</li>`;
         textInput.value = ''; // clear the input
+        textInput.classList.remove('is-invalid');
+        // help.classList.add('hide');
     } else {
         // Bad input 😢
-
+        // help.classList.remove('hide');
+        textInput.classList.add('is-invalid');
     }
+    textInput.focus();
 }
 
 myForm.addEventListener('submit', handleSubmit);
