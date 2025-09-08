6a5251380a3e16d3329440dce8b781df0cfaf89f
Dan Gilleland <dgilleland@nait.ca>
2025-09-08 11:32:00 -0600

    First JS Review demo

--- src/A/review-break-activities-example-START/js/index.js
+++ src/A/review-break-activities-example-START/js/index.js
@@ -6,3 +6,25 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+const myForm = document.querySelector('form');
+
+const handleSubmit = function(evt) {
+    // client-side only
+    evt.preventDefault(); // stop the POST to the webserver
+    console.log('Form submit event', evt);
+    const targetForm = evt.target;
+    const textInput = targetForm.elements['new-topic'];
+    console.log('The user typed:', textInput.value);
+
+    if(textInput.value.trim() !== '') {
+        // Good, I have text...
+        const list = document.querySelector('ul');
+        list.innerHTML += `<li>${textInput.value.trim()}</li>`;
+        textInput.value = ''; // clear the input
+    } else {
+        // Bad input 😢
+
+    }
+}
+
+myForm.addEventListener('submit', handleSubmit);
