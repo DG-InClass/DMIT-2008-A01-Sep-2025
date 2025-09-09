8c5a00922d1ab57e9b69b8ab527bf816f078ccf9
Dan Gilleland <dgilleland@nait.ca>
2025-09-09 13:49:38 -0600

    Getting valid JSON from API

--- src/A/review-astronauts-example-START/js/dom/astronaut.js
+++ src/A/review-astronauts-example-START/js/dom/astronaut.js
@@ -12,3 +12,18 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
     <p class="mb-1">ASTRONAUT BIO HERE </p>
 </li>
 */
+const renderAstronautListItem = function(astronautData, listElement) {
+    const listItem = `<li href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+        <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">    
+        <h5 class="mb-1">${astronautData.name} (ASTRONAUT STATUS HERE)</h5>
+        <small class="float-end">born DATE OF BIRTH HERE</small>
+    </div>
+    <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>    
+    <p class="mb-1">ASTRONAUT BIO HERE </p>
+</li>`;
+    listElement.innerHTML += listItem;
+}
+
+export { renderAstronautListItem }
+
--- src/A/review-astronauts-example-START/js/index.js
+++ src/A/review-astronauts-example-START/js/index.js
@@ -1,2 +1,14 @@
 // js here.
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getAstronautList } from './api/astronaut';
+import { renderAstronautListItem } from './dom/astronaut';
+
+const unorderedList = document.querySelector('ul');
+
+getAstronautList().then(data => {
+    // console.log(data);
+    data.results.forEach(item => {
+        console.log(item);
+        renderAstronautListItem(item, unorderedList);
+    })
+});
