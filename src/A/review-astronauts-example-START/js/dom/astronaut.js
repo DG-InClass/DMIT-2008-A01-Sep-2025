1858467eff9d90efc92454b19b9cf31b459ea617
Dan Gilleland <dgilleland@nait.ca>
2025-09-09 14:06:38 -0600

    Completed astronaut sample
    More can be done to improve the dom/astronaut.js implementation

--- src/A/review-astronauts-example-START/js/dom/astronaut.js
+++ src/A/review-astronauts-example-START/js/dom/astronaut.js
@@ -13,14 +13,18 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
 </li>
 */
 const renderAstronautListItem = function(astronautData, listElement) {
+    let status = astronautData.status.name === 'Active' ? 'Active' : 'Retired';
+    if(astronautData.in_space) {
+        status += ' | Currently in Space'
+    }
     const listItem = `<li href="#" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
-        <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">    
-        <h5 class="mb-1">${astronautData.name} (ASTRONAUT STATUS HERE)</h5>
-        <small class="float-end">born DATE OF BIRTH HERE</small>
+        <img src="${astronautData.profile_image_thumbnail}" class="rounded float-start" alt="${astronautData.name} thumbnail">    
+        <h5 class="mb-1">${astronautData.name} (${status})</h5>
+        <small class="float-end">born ${astronautData.date_of_birth}</small>
     </div>
-    <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>    
-    <p class="mb-1">ASTRONAUT BIO HERE </p>
+    <small>${astronautData.nationality} (${astronautData.agency.name})</small>    
+    <p class="mb-1">${astronautData.bio}</p>
 </li>`;
     listElement.innerHTML += listItem;
 }
