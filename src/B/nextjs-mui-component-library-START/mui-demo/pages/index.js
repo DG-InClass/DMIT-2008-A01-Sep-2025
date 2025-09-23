4a1c983f04ab63d691172d1ca3e80f100e153398
Dan Gilleland <dgilleland@nait.ca>
2025-09-23 14:43:43 -0600

    Quick demo of what MUI looks like
    And yes, it does look good!

--- src/B/nextjs-mui-component-library-START/mui-demo/pages/index.js
+++ src/B/nextjs-mui-component-library-START/mui-demo/pages/index.js
@@ -13,6 +13,8 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+import { Container, Box, Typography } from "@mui/material";
+
 export default function Home() {
   return (
     <>
@@ -26,6 +28,17 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
+          <Container maxWidth="sm">
+            <Box sx={{ my: 4 }}>
+              {/* we'll add some content here.*/}
+              <Typography variant="h2" component="h2">
+                MUI looks good.
+              </Typography>
+              <Typography variant="p" component="p">
+                You can perhaps see why this is a popular package.
+              </Typography>         
+            </Box>
+          </Container>
         </main>
       </div>
     </>
