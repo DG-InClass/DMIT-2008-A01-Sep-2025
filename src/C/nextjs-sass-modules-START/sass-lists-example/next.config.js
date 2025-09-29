928374318091099d2b8c39afd27b65c24419ce0f
Dan Gilleland <dgilleland@nait.ca>
2025-09-29 11:14:32 -0600

    All the steps for the MUI SASS ReadMe

--- src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
+++ src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
@@ -1,6 +1,14 @@
+const path = require('path'); // This is the CommonJS way of "importing"
+// import { path } from 'path'; // This is ESM/ES6 of doing importing
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    // __dirname is a Global Variable that helps identify the root
+    includePaths: [path.join(__dirname, 'styles')],
+  }
 }
 
 module.exports = nextConfig
