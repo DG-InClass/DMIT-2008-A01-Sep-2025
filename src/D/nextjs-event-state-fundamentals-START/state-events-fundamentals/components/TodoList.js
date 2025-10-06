0356af5feed9da1b32883d6b09e9fd01d0ef26de
Dan Gilleland <dgilleland@nait.ca>
2025-10-06 10:44:58 -0600

    Introduce the concept of State with React components
    This is know as adding a 'state hook'

--- src/D/nextjs-event-state-fundamentals-START/state-events-fundamentals/components/TodoList.js
+++ src/D/nextjs-event-state-fundamentals-START/state-events-fundamentals/components/TodoList.js
@@ -1,14 +1,25 @@
 // ~/components/TodoList.js
 import { Box, Button, Grid, TextField } from '@mui/material';
+import {useState} from 'react'; // useState is a "hook"
 
 // Create an event handler
-const onTodoTextChange = (event) => {
-    console.log(event.target.value);
+
+const onAddTodoClick = () => {
+    console.log('clicked');
 }
 
 export default function TodoList(props) {
+    // The following is state that our component will manage
+    const [todoText, setTodoText] = useState("");
+    const onTodoTextChange = (event) => {
+        console.log(event.target.value);
+        setTodoText(event.target.value);
+    }
+
     return <>
-        <TextField id="standard-basic" label="New Todo?" variant="standard"
-           sx={{width: '100%'}} onChange={onTodoTextChange} />
+        <TextField id="standard-basic" label="New Todo?"
+           variant="standard" sx={{width: '100%'}}
+           onChange={onTodoTextChange} value={todoText} />
+        <Button variant='contained' onClick={onAddTodoClick}>Add Todo</Button>
     </>
 }
