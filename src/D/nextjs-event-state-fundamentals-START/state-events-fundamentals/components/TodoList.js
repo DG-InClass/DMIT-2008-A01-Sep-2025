c0ecd2d4a4ad37af263f8f2fce83e3890fd6a1f5
Dan Gilleland <dgilleland@nait.ca>
2025-10-06 10:21:46 -0600

    Add an event listener for the onChange event

--- src/D/nextjs-event-state-fundamentals-START/state-events-fundamentals/components/TodoList.js
+++ src/D/nextjs-event-state-fundamentals-START/state-events-fundamentals/components/TodoList.js
@@ -1,4 +1,14 @@
 // ~/components/TodoList.js
 import { Box, Button, Grid, TextField } from '@mui/material';
 
+// Create an event handler
+const onTodoTextChange = (event) => {
+    console.log(event.target.value);
+}
 
+export default function TodoList(props) {
+    return <>
+        <TextField id="standard-basic" label="New Todo?" variant="standard"
+           sx={{width: '100%'}} onChange={onTodoTextChange} />
+    </>
+}
